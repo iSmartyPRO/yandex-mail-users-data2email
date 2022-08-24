c4ea6362c3d143132ebe4ca8f1d5ff1622faa059
Ilias Aidar <ilias.aidar@gencoindustry.com>
2022-08-24 13:16:48 +0300

    added table header

--- index.ps1
+++ index.ps1
@@ -7,8 +7,8 @@ $all_users = Get-YaEmailList
 $active_users = $all_users | ? {$_.isEnabled -eq $true} | select id, lastName, firstName, nickname, email, position, gender, createdAt, updatedAt
 $inactive_users = $all_users | ? {$_.isEnabled -eq $false} | select id, lastName, firstName, nickname, email, position, gender, createdAt, updatedAt
 
-$active_users_html = ConvertTo-HtmlTable -header ('ID', 'Фамилия', 'Имя', 'Электронная почта', 'Должность', 'Пол', 'Дата создания', 'Дата обновления') -dataKeys ('id', 'lastName', 'firstName', 'email', 'position', 'gender', 'createdAt', 'updatedAt') -dataValues $active_users
-$inactive_users_html = ConvertTo-HtmlTable -header ('ID', 'Фамилия', 'Имя', 'Электронная почта', 'Должность', 'Пол', 'Дата создания', 'Дата обновления') -dataKeys ('id', 'lastName', 'firstName', 'email', 'position', 'gender', 'createdAt', 'updatedAt') -dataValues $inactive_users
+$active_users_html = ConvertTo-HtmlTable -header ($config.tableHeaders) -dataKeys ('id', 'lastName', 'firstName', 'email', 'position', 'gender', 'createdAt', 'updatedAt') -dataValues $active_users
+$inactive_users_html = ConvertTo-HtmlTable -header ($config.tableHeaders) -dataKeys ('id', 'lastName', 'firstName', 'email', 'position', 'gender', 'createdAt', 'updatedAt') -dataValues $inactive_users
 
 $template = Get-Content ".\templates\yaEmails.html" -Encoding "UTF8"
 
